48bd9833d3d3a474fd0467194ffeaeb0f8a31395
johndaniel1 <johndaniel.v2011@vitalum.ac.in>
2020-12-16 20:43:42 +0530

    1 file modified

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, ChangeDetectionStrategy, OnInit } from "@angular/core";
 import { Console } from "@angular/core/src/console";
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-login",
@@ -34,13 +34,21 @@ import { FormBuilder } from "@angular/forms";
               />
             </mat-form-field>
 
-            <button type="submit" mat-raised-button color="primary">
+            <button type="submit" [disabled]="!loginForm.valid" mat-raised-button color="primary">
               Calculate
+            </button>
+            <button
+              (click)="reset()"
+              type="button"
+              mat-raised-button
+              color="green"
+            ><b>
+              Reset</b>
             </button>
           </form>
         </mat-card>
         <br />
-        <mat-card *ngIf="pureweight">
+        <mat-card *ngIf="pureweight > 0">
           <label
             ><b>{{ pureweight }}</b></label
           >
@@ -52,23 +60,33 @@ import { FormBuilder } from "@angular/forms";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class loginComponent implements OnInit {
-  loginForm = this.fb.group({
-    username: [null],
-    password: [null]
-  });
-
+  
+  loginForm: FormGroup;
   pecentile = 0;
   totalweight = 0;
   pureweight: number;
   toggle = false;
 
   constructor(private fb: FormBuilder) {}
-  ngOnInit() {}
+  ngOnInit() {
+    this.loginForminit();
+  }
+  loginForminit(){
+    this.loginForm = this.fb.group({
+    username: [null,Validators.required],
+    password: [null,Validators.required]
+  });
+  }
 
   onSubmit() {
     this.toggle = !this.toggle;
     this.pecentile = +this.loginForm.value.username;
     this.totalweight = +this.loginForm.value.password;
     this.pureweight = (this.pecentile * this.totalweight) / 100;
+  }
+  reset() {
+    console.log("hi");
+    this.pureweight=0;
+    this.loginForm.reset;
   }
 }
